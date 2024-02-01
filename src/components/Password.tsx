124bcdf3e43cbdaaf0da3106edb040fd4cac806a
Iliatsiklauri <tsiklauri.ilia26@gmail.com>
2024-02-01 22:36:50 +0400

    updated

--- src/components/Password.tsx
+++ src/components/Password.tsx
@@ -16,7 +16,7 @@ const Password = ({ text }: PropsType) => {
     <div className="w-[343px] h-[50px] border-2 border-black flex items-center justify-around wrap">
       <h1 className="pass w-[280px]">{text}</h1>
       <div className="cursor-pointer" onClick={handleCopyClick}>
-        <img src="public\Shape (1).png" alt="" />
+        <img src="public/Shape (1).png" alt="" />
       </div>
     </div>
   );
